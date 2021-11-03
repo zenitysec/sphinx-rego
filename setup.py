b536e31cb8f4cddafdc63723e760fa3fbfcf8578
mibarg <11074433+mibarg@users.noreply.github.com>
2021-11-03 11:34:25 +0200

    minor should start at one

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sphinx-rego",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "docutils"
